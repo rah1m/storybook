92d85c1c7f48a718cf9e4467ae9d7582c2d41284
rahimfirudunlu <rahim@workstreams.ai>
2022-05-15 10:35:38 +0400

    fix(button): pretty button comm=ponent

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,28 +1,48 @@
 import { FC, ButtonHTMLAttributes, ReactNode } from "react";
 import cn from "classnames";
 
-import buttonStyles from './Button.module.scss'
+import buttonStyles from "./Button.module.scss";
 
-
-type ButtonColor = "primary" | "secondary" | "danger" | "success" | "warning" | "info" | "light" | "dark";
+type ButtonColor =
+  | "primary"
+  | "secondary"
+  | "danger"
+  | "success"
+  | "warning"
+  | "info"
+  | "light"
+  | "dark";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
-  color?: ButtonColor
+  color?: ButtonColor;
   outlined?: boolean;
 }
-const Button: FC<ButtonProps> = ({ onClick, children, color = "primary", outlined = false, ...props }) => {
-  return <button {...props} className={cn(buttonStyles.button, {
-    [buttonStyles.button__primary]: color === "primary" && !outlined,
-    [buttonStyles.button__secondary]: color === "secondary" && !outlined,
-    [buttonStyles.button__danger]: color === "danger" && !outlined,
-    [buttonStyles.button__success]: color === "success" && !outlined,
-    [buttonStyles.button__warning]: color === "warning" && !outlined,
-    [buttonStyles.button__info]: color === "info" && !outlined,
-    [buttonStyles.button__light]: color === "light" && !outlined,
-    [buttonStyles.button__dark]: color === "dark" && !outlined,
-    [buttonStyles.button__outlined]: outlined
-  })}>{children}</button>;
+const Button: FC<ButtonProps> = ({
+  onClick,
+  children,
+  color = "primary",
+  outlined = false,
+  ...props
+}) => {
+  return (
+    <button
+      {...props}
+      className={cn(buttonStyles.button, {
+        [buttonStyles.button__primary]: color === "primary" && !outlined,
+        [buttonStyles.button__secondary]: color === "secondary" && !outlined,
+        [buttonStyles.button__danger]: color === "danger" && !outlined,
+        [buttonStyles.button__success]: color === "success" && !outlined,
+        [buttonStyles.button__warning]: color === "warning" && !outlined,
+        [buttonStyles.button__info]: color === "info" && !outlined,
+        [buttonStyles.button__light]: color === "light" && !outlined,
+        [buttonStyles.button__dark]: color === "dark" && !outlined,
+        [buttonStyles.button__outlined]: outlined,
+      })}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
